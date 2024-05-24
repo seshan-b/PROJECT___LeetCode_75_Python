02d81766ccad1d7134fac407b4f22dcf1a22b161
Seshan-B <seshan.b@gmail.com>
2024-05-24 18:41:18 +1200

    Clean up files

--- Leet Code 75 Python/Merge_Strings_Alternately/test.py
+++ Leet Code 75 Python/Merge_Strings_Alternately/test.py
@@ -8,24 +8,21 @@ def test_merge_alternately():
     word1 = "abc"
     word2 = "pqr"
     expected_output = "apbqcr"
-    assert solution.mergeAlternately(word1,
-                                     word2) == expected_output, f"Test Case 1 Failed: expected {expected_output}, got {solution.mergeAlternately(word1, word2)}"
+    assert solution.mergeAlternately(word1, word2) == expected_output, f"Test Case 1 Failed: expected {expected_output}, got {solution.mergeAlternately(word1, word2)}"
     print("Test Case 1 Passed")
 
     # Test Case 2
     word1 = "ab"
     word2 = "pqrs"
     expected_output = "apbqrs"
-    assert solution.mergeAlternately(word1,
-                                     word2) == expected_output, f"Test Case 2 Failed: expected {expected_output}, got {solution.mergeAlternately(word1, word2)}"
+    assert solution.mergeAlternately(word1, word2) == expected_output, f"Test Case 2 Failed: expected {expected_output}, got {solution.mergeAlternately(word1, word2)}"
     print("Test Case 2 Passed")
 
     # Test Case 3
     word1 = "abcd"
     word2 = "pq"
     expected_output = "apbqcd"
-    assert solution.mergeAlternately(word1,
-                                     word2) == expected_output, f"Test Case 3 Failed: expected {expected_output}, got {solution.mergeAlternately(word1, word2)}"
+    assert solution.mergeAlternately(word1, word2) == expected_output, f"Test Case 3 Failed: expected {expected_output}, got {solution.mergeAlternately(word1, word2)}"
     print("Test Case 3 Passed")
 
 
